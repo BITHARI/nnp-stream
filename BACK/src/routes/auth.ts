ae58a3a505cbf8afa904ef8ba9cb022834b7d6d2
BITHARI <yengayengavital@gmail.com>
2026-02-12 16:20:46 +0200

    Refactor auth routes

--- BACK/src/routes/auth.ts
+++ BACK/src/routes/auth.ts
@@ -1,18 +1,17 @@
 import { authenticate } from "../middleware/auth.js";
-import { changePassword, getAuth, resetPassword, setNewPassword, signIn, signOut, signUp, updateProfile, verifyEmail } from "../controllers/auth.js";
+import * as authController from "../controllers/auth.js";
 import { Router } from "express";
 
 const router = Router()
 
-router.get("", getAuth)
-router.post("/sign-in", signIn)
-router.get("/sign-out", authenticate, signOut)
-router.post("/sign-up", signUp)
-
-router.post("/update-profile", authenticate, updateProfile)
-router.post("/change-password", authenticate, changePassword)
-router.post("/reset-password", resetPassword)
-router.post("/set-new-password", setNewPassword)
-router.post("/verify-email", verifyEmail)
+router.get("", authController.getAuth)
+router.post("/sign-in", authController.signIn)
+router.get("/sign-out", authenticate, authController.signOut)
+router.post("/sign-up", authController.signUp)
+router.post("/update-profile", authenticate, authController.updateProfile)
+router.post("/change-password", authenticate, authController.changePassword)
+router.post("/reset-password", authController.resetPassword)
+router.post("/set-new-password", authController.setNewPassword)
+router.post("/verify-email", authController.verifyEmail)
 
 export default router
